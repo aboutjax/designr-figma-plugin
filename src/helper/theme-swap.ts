d98fe87bb57c804142cdd1d660938055bb15a0b1
Jacky Lee <aboutjax1990@gmail.com>
2021-06-07 22:51:08 -0400

    add focus styles

--- src/helper/theme-swap.ts
+++ src/helper/theme-swap.ts
@@ -121,7 +121,8 @@ const swapEffects = async (node) => {
 
 // Theme Swap
 const ThemeSwap = async (node) => {
-  GetAllLocalPaintStyles();
+  // GetAllLocalPaintStyles();
+  GetAllLocalEffectStyles();
   // do nothing below
   if ("children" in node) {
     for (const child of node.children) {
--- src/theme-styles/effects.js
+++ src/theme-styles/effects.js
@@ -1,77 +1,37 @@
 export const LightUiEffects = [
   {
-    name: "light ui / border / top",
-    styleKey: "b48722d61648d7c6565fea640361853ec53639d1",
-  },
-  {
-    name: "light ui / border / right",
-    styleKey: "3394fee8e3f08edc7d60d6aab29f0f3f8ee1b1b2",
-  },
-  {
-    name: "light ui / border / bottom",
-    styleKey: "f63fff39ae021f0779b6d46dc975bc578dd3b7c5",
-  },
-  {
-    name: "light ui / border / left",
-    styleKey: "9edddf780bf44515d95e2a4bff6036e247da5af5",
-  },
-  {
-    name: "light ui / border / top & bottom",
-    styleKey: "23c11916e6af413ef220acfcc40195d82fe303b5",
-  },
-  {
-    name: "light ui / border / left & right",
-    styleKey: "a6447b7210901bca45b98fe918ac55540fa8354e",
-  },
-  {
-    name: "light ui / shadow / elevation 4",
+    name: "light ui/shadow/elevation 4",
     styleKey: "69202fa8cc4f6cc0515cc08fdcc988459af28731",
   },
   {
-    name: "light ui / shadow / elevation 9",
+    name: "light ui/shadow/elevation 9",
     styleKey: "8ccea9d523cf9dc13fee68f392f3f9cc3187a457",
   },
   {
-    name: "light ui / shadow / elevation 14",
+    name: "light ui/shadow/elevation 14",
     styleKey: "c61253c38f6d0d9f3b302a77a3807e052e9c3f91",
+  },
+  {
+    name: "light ui/focus",
+    styleKey: "f9d539f6702b711166b89743e597101be6153b9c",
   },
 ];
 
 export const DarkUiEffects = [
   {
-    name: "dark ui / border / top",
-    styleKey: "d394c6ff78a94c900f344b53f1d7e0e7ecdccd79",
-  },
-  {
-    name: "dark ui / border / right",
-    styleKey: "433c605f34e9a88d0350f22fe58c9afc8f0e7dd7",
-  },
-  {
-    name: "dark ui / border / bottom",
-    styleKey: "3e46748105106b65440c24f1b9e41c994fdac402",
-  },
-  {
-    name: "dark ui / border / left",
-    styleKey: "f88b227dd6d6b4f8285b2e179a7c7d4277f0fb31",
-  },
-  {
-    name: "dark ui / border / top & bottom",
-    styleKey: "a34f3d0a22c2459cfc626f178c47d9bb1ba8b2a5",
-  },
-  {
-    name: "dark ui / border / left & right",
-    styleKey: "fc06b8e7032fc9b1fa07af5e39293df2150ff4dc",
-  },
-  {
-    name: "dark ui / shadow / elevation 4",
+    name: "dark ui/shadow/elevation 4",
     styleKey: "a89abbe6d64f824f62ebb1f54f6f28a910490195",
   },
   {
-    name: "dark ui / shadow / elevation 9",
+    name: "dark ui/shadow/elevation 9",
     styleKey: "98761904bcc9d38c81ee23e7b432d8c97e29f0c1",
   },
   {
-    name: "dark ui / shadow / elevation 14",
+    name: "dark ui/shadow/elevation 14",
     styleKey: "209b451e4f68f5bf3ade24ef36a10a609cdae239",
+  },
+  {
+    name: "dark ui/focus",
+    styleKey: "92cbe36d4480efe4b9618ec6ec1a4449241fbdd6",
   },
 ];
